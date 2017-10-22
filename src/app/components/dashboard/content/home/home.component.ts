3d7cf1133f5b6eaeccdd94c0ff34347c7b4522ff
ShahPar <shhrrparvez@gmail.com>
2017-10-22 13:24:08 -0400

    Basic home page, styling adjustments

--- src/app/components/dashboard/content/home/home.component.ts
+++ src/app/components/dashboard/content/home/home.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  recents: number[];
   constructor() { }
 
   ngOnInit() {
+    this.recents = [1,2,3,4,5,6]
   }
 
 }
