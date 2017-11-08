b40a8bc887ab9f59ccb756421f295a2ef8f0456f
ShahPar <shhrrparvez@gmail.com>
2017-11-08 16:26:14 -0500

    added retrieval for customer songs

--- src/app/services/library/library.service.ts
+++ src/app/services/library/library.service.ts
@@ -3,13 +3,15 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { DataService } from '../data.service';
 
+
 @Injectable()
 export class LibraryService {
 
   constructor(private http: Http, public dataService: DataService) { }
 
   getAllSongs() {
-    return this.http.get(this.dataService.connectionURL + "/songs").map(res => res.json());
+    let accountId = JSON.parse(sessionStorage.getItem("currentUser"))._accountId;
+    return this.http.get(this.dataService.connectionURL + "/accounts/" + accountId + "/songs").map(res => res.json());
   }
   getAllPlaylists() {
     return this.http.get(this.dataService.connectionURL + "/playlists").map(res => res.json());
