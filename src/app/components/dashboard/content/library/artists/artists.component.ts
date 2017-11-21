d063acc12ea454d20b32400e40c67f0ad8b19288
ShahPar <shhrrparvez@gmail.com>
2017-11-21 05:57:25 -0500

    added some artist images

--- src/app/components/dashboard/content/library/artists/artists.component.ts
+++ src/app/components/dashboard/content/library/artists/artists.component.ts
@@ -3,6 +3,7 @@ import { Router } from "@angular/router";
 import { Artist } from '../../../../../classes/Artist';
 import { GeneralService } from '../../../../../services/general/general.service';
 import { PlayerService } from '../../../../../services/player/player.service';
+import { DataService } from '../../../../../services/data.service';
 
 @Component({
   selector: 'app-artists',
@@ -10,15 +11,18 @@ import { PlayerService } from '../../../../../services/player/player.service';
   styleUrls: ['./artists.component.css']
 })
 export class ArtistsComponent implements OnInit {
+  mediaPath: string;
   accountId: number;
   artists: Artist[]
   constructor(
     private router: Router,
     private generalService: GeneralService,
-    private playerService: PlayerService
+    private playerService: PlayerService,
+    private dataService: DataService
   ) {}
 
   ngOnInit() {
+    this.mediaPath = this.dataService.mediaURL;
     this.accountId = JSON.parse(sessionStorage.getItem("currentUser"))._accountId;
     this.generalService.get("/accounts/" + this.accountId + "/artists").subscribe((artists) => {
       this.artists = artists
