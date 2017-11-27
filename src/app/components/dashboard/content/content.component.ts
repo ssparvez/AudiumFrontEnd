0d806da5976a9824c6f00a9eafda052fefb7d914
ShahPar <shhrrparvez@gmail.com>
2017-11-27 18:16:21 -0500

    added small upgrade banner

--- src/app/components/dashboard/content/content.component.ts
+++ src/app/components/dashboard/content/content.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { CustomerAccount } from '../../../classes/CustomerAccount';
+import { PaymentInfoComponent } from '../../../modals/payment-info/payment-info.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-content',
@@ -7,14 +10,26 @@ import { Router } from "@angular/router";
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  currentUser: CustomerAccount;
+  isHidden = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private dialog: MatDialog) { }
 
   ngOnInit() {
+    this.currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+    console.log(this.currentUser)
   }
 
   onEnter(keywords: string) {
     this.router.navigate(['/dash/search/', keywords]);
     console.log(keywords);
   }
+
+  openNewPaymentDialog() {
+    
+        this.dialog.open(PaymentInfoComponent,{ data: {isNew: true}, width: '400px'}, )
+          .afterClosed()
+          .subscribe(result => {
+          });
+      }
 }
