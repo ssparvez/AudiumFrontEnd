b398ee2f4da04a8498002c7c5caf45be3f3b7db4
SeanPesce <pesce.sean@gmail.com>
2017-12-05 19:52:57 -0500

    Add new member data for Genre class (needed for genre pages)

--- src/app/classes/Genre.ts
+++ src/app/classes/Genre.ts
@@ -1,4 +1,9 @@
+import { Artist } from './Artist'
+import { Song } from './Song'
+
 export interface Genre {
     genreId?: number;
     genreName?: string;
+    songs?: Song[];
+    artists?: Artist[];
 }
