38c7e7c1a9027440acb48682147b94f385cc6964
ShahPar <shhrrparvez@gmail.com>
2017-12-28 23:29:07 -0500

    updated connection url

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataService {
-    connectionURL = "http://localhost:8080";
+    // connectionURL = "http://localhost:8080" // local dev 
+    connectionURL = "http://audium-api.us-east-2.elasticbeanstalk.com/";
     mediaURL = "https://s3.us-east-2.amazonaws.com/assets.audium.io";
     songUrl = "https://s3.us-east-2.amazonaws.com/assets.audium.io/audio/";
     albumImg = "https://s3.us-east-2.amazonaws.com/assets.audium.io/images/album_arts/";
